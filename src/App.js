6c3450de29ba0881edab3deae97c882fd3d3d5af
samiracode <samiratavakolli.k@gmail.com>
2023-08-08 14:31:40 +0200

    creating categories divs in Appjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 
-import Home from './routes/home/home.component';
-import { Routes, Route } from 'react-router-dom';
-import Navigation from './routes/navigation/navigation.component';
-
-const Shop = () => {
-  return (
-    <h1>I am the Shop Page</h1>
-  )
-}
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />} >
-        <Route index element={<Home />}/>
-        <Route path='/shop' element={<Shop />}/>
-      </Route>  
-    </Routes>
+    <div className="categories-container">
+      
+      <div className="category-container">
+       { /*<img />*/}
+       <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Here!</p>
+       </div>
+      </div>
+
+      <div className="category-container">
+       { /*<img />*/}
+       <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Here!</p>
+       </div>
+      </div>
+
+      <div className="category-container">
+       { /*<img />*/}
+       <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Here!</p>
+       </div>
+      </div>
+
+      <div className="category-container">
+       { /*<img />*/}
+       <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Here!</p>
+       </div>
+      </div>
+
+      <div className="category-container">
+       { /*<img />*/}
+       <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Here!</p>
+       </div>
+      </div>
+
+
+    </div>
+
   );
 };
 
